4873a255896858a594433cfe7a6a879c91c1254c
Óscar <Xxracso40xX@gmail.com>
2018-11-27 13:24:56 +0100

    Botón 1

--- app/src/main/java/com/example/pc/practicatema4pmdm/MainActivity.java
+++ app/src/main/java/com/example/pc/practicatema4pmdm/MainActivity.java
@@ -8,19 +8,28 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    Button boton;
     Button boton1;
+    Button boton2;
+    Intent intent2;
     Intent intent;
+   Intent intent1;
+    Intent intent3;
     @Override
     //  AppCompatActivity estaActivity;
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intent = new Intent(this, DesplazandoImagenes.class);
+        intent2 = new Intent(this, DesplazandoImagenes.class);
+        intent = new Intent(this, activity_boton1.class);
+       // intent = new Intent(this, activity_boton2.class);
+     //   intent = new Intent(this, activity_boton4.class);
       //  estaActivity = this;
-        boton1 = findViewById(R.id.button2);
-        boton1.setOnClickListener(new View.OnClickListener()
+        boton=findViewById(R.id.button);
+        boton1=findViewById(R.id.button1);
+        boton2 = findViewById(R.id.button2);
+        boton2.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
@@ -30,10 +39,27 @@ public class MainActivity extends AppCompatActivity
 
             private void openActivity()
             {
+                //Botónn 3 del programa
                 //Intent intent = new Intent(estaActivity, DesplazandoImagenes.class);
                 //startActivity(intent);
+                startActivity(intent2);
+            }
+        });
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //Botón 1 del programa
                 startActivity(intent);
             }
         });
+        boton1.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v)
+        {
+            //Botón 2 del programa
+            startActivity(intent1);
+        }
+    });
     }
 }
---
+++ app/src/main/java/com/example/pc/practicatema4pmdm/activity_boton1.java
@@ -0,0 +1,48 @@
+package com.example.pc.practicatema4pmdm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class activity_boton1 extends AppCompatActivity {
+
+    Button boton;
+    TextView txtv;
+    EditText edit;
+    Boolean isPrime;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boton1);
+        boton = findViewById(R.id.button4);
+        txtv = findViewById(R.id.textView2);
+        edit = findViewById(R.id.editText);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero;
+                numero =Integer.parseInt(edit.getText().toString());
+                isPrime = true;
+                for(int divisor = 2; divisor <= numero / 2; divisor++) {
+                    if (numero % divisor == 0) {
+                        isPrime = false;
+                        break; // num is not a prime, no reason to continue checking
+                    }
+                }
+                if(isPrime==true)
+                {
+                    txtv.setText("El número es primo");
+                }
+                else
+                {
+                    txtv.setText("El número no es primo");
+                }
+            }
+        });
+    }
+
+}
